05c4b663e0f7daad09dfeba1cb7562a9a4adbaa1
jayao1030 <67378545+jayao1030@users.noreply.github.com>
2021-03-25 22:40:25 +0800

    upload file

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+});
+
+export const db = firebase.firestore();
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
